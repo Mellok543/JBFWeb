b9bbfae6a85d9720458478e2f0107e9a2a96e2e6
Mellok543 <73647025+Mellok543@users.noreply.github.com>
2026-09-23 23:45:58 +0300

    Add admin player search repository query

--- backend/src/main/java/com/jbforsaken/web/user/WebUserRepository.java
+++ backend/src/main/java/com/jbforsaken/web/user/WebUserRepository.java
@@ -1,6 +1,8 @@
 package com.jbforsaken.web.user;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface WebUserRepository extends JpaRepository<WebUser, Long> {
+    List<WebUser> findTop100ByNicknameContainingIgnoreCaseOrderByLastLoginAtDesc(String nickname);
 }
