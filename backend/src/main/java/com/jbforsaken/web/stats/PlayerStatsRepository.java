da35516293f0f45ca1ddc08e3332dd5b4f977591
Mellok543 <73647025+Mellok543@users.noreply.github.com>
2026-09-23 22:41:27 +0300

    Add portal backend module: backend/src/main/java/com/jbforsaken/web/stats/PlayerStatsRepository.java

---
+++ backend/src/main/java/com/jbforsaken/web/stats/PlayerStatsRepository.java
@@ -0,0 +1,5 @@
+package com.jbforsaken.web.stats;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlayerStatsRepository extends JpaRepository<PlayerStats, Long> {}
