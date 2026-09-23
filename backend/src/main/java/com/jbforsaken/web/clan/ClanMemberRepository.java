337866fe06d884b7337b44e7ef426e04255ac53e
Mellok543 <73647025+Mellok543@users.noreply.github.com>
2026-09-23 23:25:06 +0300

    Support deleting clan members with clan

--- backend/src/main/java/com/jbforsaken/web/clan/ClanMemberRepository.java
+++ backend/src/main/java/com/jbforsaken/web/clan/ClanMemberRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface ClanMemberRepository extends JpaRepository<ClanMember, Long> {
     Optional<ClanMember> findBySteamId64(Long steamId64);
     long countByClanId(Long clanId);
+    void deleteByClanId(Long clanId);
 }
